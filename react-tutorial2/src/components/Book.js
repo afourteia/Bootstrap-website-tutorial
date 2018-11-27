0d7e26ffcd7e2431d19ccca1acf47d7eeef62f83
AbdulRaoph Fourteia (Abdul) <12877426+afourteia@users.noreply.github.com>
2018-11-27 14:08:45 -0800

    another condition expression

--- react-tutorial2/src/components/Book.js
+++ react-tutorial2/src/components/Book.js
@@ -19,6 +19,19 @@ export default class Book extends Component {
     render() {
         const { id, img, title, author } = this.props.info;
         const { deleteItem } = this.props;
+        const checkInfo = info => {
+            if (info === true) {
+                return (
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Necessitatibus, aperiam. Tenetur quas in
+                        molestiae!
+                    </p>
+                );
+            } else {
+                return null;
+            }
+        };
         return (
             <div className="book">
                 <img src={img} alt="book" width="150px" />
@@ -27,12 +40,7 @@ export default class Book extends Component {
                     <h6>By {author}</h6>
                     <button onClick={() => deleteItem(id)}>Delete item</button>
                     <button onClick={this.handleInfo}>Show Info</button>
-                    {this.state.showInfo ? (
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Amet consectetur molestiae nostrum id placeat!
-                        </p>
-                    ) : null}
+                    {checkInfo(this.state.showInfo)}
                 </div>
             </div>
         );

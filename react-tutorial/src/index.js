2a2c589a3808c92a58b1622c17ddd401903f6f17
AbdulRaoph Fourteia (Abdul) <12877426+afourteia@users.noreply.github.com>
2018-11-26 21:37:35 -0800

    using regular javascript

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -2,48 +2,21 @@ import React from "react";
 import ReactDom from "react-dom";
 
 // Import CSS
-import "./index.css";
+// import "./index.css";
 
 // Use JXS sytax
 function Greeting() {
+    const btn = "search button";
+    const name = "john";
+    const lastname = "four";
+
     return (
-        <div>
-            <h1>hello from React</h1>
-            <p>lorem7</p>
-            <ul>
-                <li>
-                    <a href="#">home</a>
-                </li>
-            </ul>
-            <section className="book">
-                <Person />
-                <Message />
-                <CoverImage />
-                <Title />
-                <Author />
-            </section>
-        </div>
+        <section>
+            <button>{btn}</button>
+            <h2>{`${name} ${lastname}`}</h2>
+            <p>{12 + 34}</p>
+        </section>
     );
 }
-
-const Person = () => <h1>Abdul Fourteia</h1>;
-const Message = () => (
-    <p style={{ backgroundColor: "white" }}>This is my message</p>
-);
-
-const CoverImage = () => (
-    <img
-        width="200"
-        src="https://images-na.ssl-images-amazon.com/images/I/51r9XYcHP+L._AA218_.jpg"
-        alt="book cover"
-    />
-);
-
-const Title = () => <h1>The subtle of something something</h1>;
-
-const Author = () => <p> By someone</p>;
-// function Greeting() {
-//   return React.createElement("div", {}, "hello world");
-// }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));

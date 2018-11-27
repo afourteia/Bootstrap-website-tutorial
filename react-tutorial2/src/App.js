c9951f4366947823132f8244a05db02d7166bdd8
AbdulRaoph Fourteia (Abdul) <12877426+afourteia@users.noreply.github.com>
2018-11-27 13:17:13 -0800

    using state

--- react-tutorial2/src/App.js
+++ react-tutorial2/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Booklist1 from "./components/Booklist";
+import Button from "./components/Button";
 
 class App extends Component {
     render() {
--- react-tutorial2/src/components/Book.js
+++ react-tutorial2/src/components/Book.js
@@ -1,6 +1,29 @@
 import React, { Component } from "react";
 
 export default class Book extends Component {
+    constructor(props) {
+        super(props);
+
+        // You have to bind manually if you use regular functiosn instead of arrow functions
+        // this.handleClick = this.handleClick.bind(this);
+
+        this.state = {
+            count: 0
+        };
+    }
+
+    // Regular function
+    // handleClick () {
+    //     console.log("you clicked me");
+    //     console.log(this.state.count);
+    // };
+
+    // Arrow function
+    handleClick = () => {
+        console.log("you clicked me");
+        console.log(this.state.count);
+    };
+
     render() {
         const { img, title, author } = this.props.info;
         return (
@@ -9,6 +32,7 @@ export default class Book extends Component {
                 <div>
                     <h4>Title: {title}</h4>
                     <h6>By {author}</h6>
+                    <button onClick={this.handleClick}>add Count</button>
                 </div>
             </div>
         );

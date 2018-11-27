c4213036067ebefc98191bc37981f3ba9ccdd21e
AbdulRaoph Fourteia (Abdul) <12877426+afourteia@users.noreply.github.com>
2018-11-26 20:50:46 -0800

    react with ES6

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+// Use JXS sytax
 function Greeting() {
-  return <div>hello from React</div>;
+    return (
+        <div>
+            <h1>hello from React</h1>
+            <p>
+                lorem7
+                <ul>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                </ul>
+            </p>
+            <Person />
+            <Message />
+        </div>
+    );
 }
+
+const Person = () => <h1>Abdul Fourteia</h1>;
+const Message = () => <p>This is my message</p>;
+
+// function Greeting() {
+//   return React.createElement("div", {}, "hello world");
+// }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));

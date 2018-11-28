debda2566aea197f026604d52b3912b27b263ef7
AbdulRaoph Fourteia (Abdul) <12877426+afourteia@users.noreply.github.com>
2018-11-27 16:54:52 -0800

    started a new city tour project

---
+++ react-city-tour-project/src/components/TourList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./TourList.scss";
+
+export default class Tourlist extends Component {
+    render() {
+        return (
+            <section>
+                <h3>from tourlist</h3>
+            </section>
+        );
+    }
+}

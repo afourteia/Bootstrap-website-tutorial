d63d2c2eb6384b01b78511f4c855e7fd4147c369
AbdulRaoph Fourteia (Abdul) <12877426+afourteia@users.noreply.github.com>
2018-11-30 21:03:05 -0800

    buttons configured

--- react-city-tour-project/src/components/Tour/Tour.js
+++ react-city-tour-project/src/components/Tour/Tour.js
@@ -2,30 +2,40 @@ import React, { Component } from "react";
 import "./Tour.scss";
 
 export default class Tour extends Component {
+    state = {
+        showInfo: false
+    };
+    handleInfo = () => {
+        this.setState({
+            showInfo: !this.state.showInfo
+        });
+    };
     render() {
+        const { id, city, img, name, info } = this.props.tour;
+        const { removeTour } = this.props;
         return (
             <article className="tour">
                 <div className="img-container">
-                    <img
-                        src="https://images.pexels.com/photos/1034859/pexels-photo-1034859.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                        alt=""
-                    />
-                    <span className="close-btn">
+                    <img src={img} alt="" />
+                    <span
+                        className="close-btn"
+                        onClick={() => {
+                            removeTour(id);
+                        }}
+                    >
                         <i className="fas fa-window-close" />
                     </span>
                 </div>
                 <div className="tour-info">
-                    <h3>city</h3>
-                    <h4>name</h4>
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
                     <h5>
-                        info <span className="fas fa-caret-square-down" />
+                        info{" "}
+                        <span onClick={this.handleInfo}>
+                            <i className="fas fa-caret-square-down" />
+                        </span>
                     </h5>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Rem beatae tempora unde nemo omnis. Magni tenetur
-                        nulla praesentium accusamus optio debitis aliquam nemo
-                        iure.
-                    </p>
+                    {this.state.showInfo && <p>{info}</p>}
                 </div>
             </article>
         );
